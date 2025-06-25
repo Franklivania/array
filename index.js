701ef6ee2830fc14e5d13bacc3dfc4e15979b56f
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2025-06-25 11:13:19 +0100

    feat: products display

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-// writing a loop
-
-function forLoop() {
-  for (let index = 6; index < 5; index++) {
-    console.log(index)
+const storeDisplay = async () => {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    console.log("Products:", data);
+    return data;
+  } catch (error) {
+    console.log(error.message);
   }
 };
 
-forLoop();
+const app = document.getElementById("app");
+async function DisplayData() {
+  const data = await storeDisplay();
+  app.innerHTML = data
+    .map(
+      (item) => `
+    <div key=${item?.id} class="product-container">
+    <div id="image-container">
+    <img src=${item?.image}>
+    </div>
+    <h3>${item?.title}</h3>
+      <section>
+        <aside>
+        <span>
+        <iconify-icon icon="emojione:star" width="24" height="24"></iconify-icon>
+        <span>${item?.rating?.rate}</span>
+        </span>
+        <small>${item?.rating?.count} ratings</small>
+        </aside>
+        <aside>
+          <h2>₦ ${item?.price}</h2>
+        </aside>
+        </section>
+        <button type="button"> Buy Now</button>
+    </div>
+  `
+    )
+    .join("");
+}
 
-// console.log(forLoop())
+DisplayData();
